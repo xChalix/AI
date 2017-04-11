9526b708320ce76e9e07987600b7fc52411841fc
Maximilian Preisinger <preisinm@aston.ac.uk>
2017-04-11 11:16:13 +0100

    comments & valid_fitness

--- GA/VC.h
+++ GA/VC.h
@@ -2,11 +2,12 @@
 #define VC_H
 
 
-class VC
+class VC //Virtual Creature
 {
 public:
     VC();
-    double fitness;
+    double fitness;             //Fitness function of the VC, must be positive! //TODO pls check in the GA every time, if the fitness IS positive. If it negative, set it to 0
+    bool valid_fitness;         //Checks if the fitness value already has been used for selection. Set to false after selection, and only start the selection when it is true.
     virtual void mutate()=0;
     virtual VC copy()=0;
 };

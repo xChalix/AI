fd1b3d352df2fc896b5556a07944468b8564ea1a
Robert Woodward <Robert Woodward>
2017-04-11 12:29:45 +0100

    created skeleton of GA::selection() function

--- GA/GA.cpp
+++ GA/GA.cpp
@@ -5,6 +5,27 @@ GA::GA()
 
 }
 
+
+/*
+*	Selects highest fitness VC objects (Virtual creatures) and zeroes out lower
+*
+*	param: VC *population
+*		pointer to array of VC objects
+*	param: int size
+*		number of Virtual Creatures in the array of VC objects
+*/
+static void GA::selection(VC *population, int size) 
+{
+	
+	sort(population,size);
+	
+	for( int i = 0; i < size; i++ )
+	{
+		
+	}
+	
+}
+
 static void GA::reproduction(VC *population, int size) {
 
 }
